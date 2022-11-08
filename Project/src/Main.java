879541f2268be840674176d21946629aeb56dea0
Riddhi3004 <70747205+riddhibhuva@users.noreply.github.com>
2022-11-08 01:01:08 -0500

    rename

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpClient;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException{
