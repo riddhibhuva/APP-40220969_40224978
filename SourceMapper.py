6484c4c1f595df86401057b79257364416a24629
AyushiChaudhary23 <117676471+AyushiChaudhary23@users.noreply.github.com>
2022-11-11 12:14:07 -0500

    Change

--- SourceMapper.py
+++ SourceMapper.py
@@ -10,7 +10,7 @@ class SourceMapper :
         self._sqlConnection.executeQuery(squery)
     
     def insertRow(self, dataObj):
-        print(dataObj)
+        #print(dataObj)
         for data in dataObj:
             squery = 'INSERT or IGNORE INTO Sources(Source_id, Source_name) VALUES('+str(data['Source_id'])+',"' + data['Source_name']+'"); '
             self._sqlConnection.executeQuery(squery)
